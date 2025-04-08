f62c816f86b369679512bff11a411bb3cd8d6fef
josesito95 <joseluisfarias117@gmail.com>
2025-04-08 16:16:55 -0300

    menu 2
    menu 2

---
+++ experiencia_ola_unity/Assets/scripts/MENU2/AdministradorLetras.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdministradorLetras : MonoBehaviour
+{
+    public GameObject letraO;
+    public GameObject letraL;
+    public GameObject letraA;
+
+    public Material materialEncendido;
+
+    private bool oActiva = false;
+    private bool lActiva = false;
+    private bool aActiva = false;
+
+    public void ActivarLetra(CuboInteractivo.LetraTipo letra)
+    {
+        switch (letra)
+        {
+            case CuboInteractivo.LetraTipo.O:
+                if (!oActiva) Activar(letraO); oActiva = true;
+                break;
+            case CuboInteractivo.LetraTipo.L:
+                if (!lActiva) Activar(letraL); lActiva = true;
+                break;
+            case CuboInteractivo.LetraTipo.A:
+                if (!aActiva) Activar(letraA); aActiva = true;
+                break;
+        }
+
+        if (oActiva && lActiva && aActiva)
+        {
+            Debug.Log("¡Logo completo!");
+            // Podés aquí mostrar un botón o animación
+        }
+    }
+
+    private void Activar(GameObject letra)
+    {
+        Renderer rend = letra.GetComponent<Renderer>();
+        rend.material = materialEncendido;
+        rend.material.EnableKeyword("_EMISSION");
+        rend.material.SetColor("_EmissionColor", Color.white * 1.5f);
+    }
+}
